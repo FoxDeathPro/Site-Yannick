a7584225fe38f74137439d7110960918a8aede94
BricePou <brice.poullain@sfr.fr>
2023-10-01 20:32:54 +0200

    beaucoup de chose

--- src/Router.js
+++ src/Router.js
@@ -4,8 +4,8 @@ import Layout from "./pages/Layout/layout";
 import Home from "./pages/Home/home";
 import Error from "./pages/Error/error";
 import Tests from "./pages/Tests/tests";
-import Maps from "./pages/Maps/maps";
 import Contact from "./pages/Contact/contact";
+import Corp from "./pages/Corp/corp";
 
 const Router = () => {
     return (
@@ -13,8 +13,9 @@ const Router = () => {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
+                    <Route path="corp" element={<Corp />} />
+                    <Route path="contact" element={<Contact />} />
                     <Route path="tests" element={<Tests />} />
-                    <Route path="maps" element={<Maps />} />
                     <Route path="contact" element={<Contact />} />
                     <Route path="*" element={<Error />} />
                 </Route>
--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -1,11 +1,85 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { validEmail, validName } from "../../components/Regex/regex";
 
-const contact = () => {
+const Contact = () => {
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [emailErr, setEmailErr] = useState(false);
+    const [nameError, setNameError] = useState(false);
+    const validate = () => {
+        if (!validEmail.test(email)) {
+            setEmailErr(true);
+        }
+        if (!validName.test(name)) {
+            setNameError(true);
+        }
+    };
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs
+            .sendForm(
+                "service_ev4rl0p",
+                "template_axvm6ht",
+                form.current,
+                "nb9gL0Ntt7Q7sqVco"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+    };
+
     return (
-        <div>
-
+        <div className="placement-card-form">
+            <div className="card-contact-form">
+                <div className="card-contact">
+                    <h1 className="title-contact">Nous Contacter</h1>
+                    <h2>Telephone :</h2>
+                    <p>06.16.45.43.04</p>
+                    <h2>Adresse :</h2>
+                    <p>62 boulevard de strasbourg</p>
+                </div>
+                <div className="card-form">
+                    <form className="form" ref={form} onSubmit={sendEmail}>
+                        <label>Nom Prénom</label>
+                        <input
+                            type="name"
+                            placeholder="Name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            className="input"
+                        />
+                        {nameError && <p>Your name is invalid</p>}
+                        <label>Email</label>
+                        <input
+                            className="input"
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        {emailErr && <p>Your email is invalid</p>}
+                        <label>Message</label>
+                        <textarea
+                            name="message"
+                            placeholder="Message..."
+                            className="message-input input"
+                        />
+                        <button
+                        className="button-send"
+                        onClick={validate}
+                        >Validé !</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default contact;
+export default Contact;
